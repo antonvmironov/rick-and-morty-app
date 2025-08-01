a741396ac471971d019b09d5c79bdc4f9028ab44
Anton Myronov <antonvmironov@gmail.com>
2025-08-01 10:21:43 +0200

    extract StringRepresentable from RickAndMortyCharacterStatus for future reuse

--- Libs/RickAndMortyEpisodesLib/Sources/RickAndMortyCharacterStatus.swift
+++ Libs/RickAndMortyEpisodesLib/Sources/RickAndMortyCharacterStatus.swift
@@ -1,7 +1,7 @@
 import Foundation
 
 /// A status of a "character" entity in "Rick and Morty app" domain.
-struct RickAndMortyCharacterStatus: Sendable, Equatable {
+struct RickAndMortyCharacterStatus: StringRepresentable {
   var rawValue: String
 
   init(rawValue: String) {
@@ -14,28 +14,4 @@ extension RickAndMortyCharacterStatus {
   static let alive: RickAndMortyCharacterStatus = "Alive"
   static let dead: RickAndMortyCharacterStatus = "Dead"
   static let unknown: RickAndMortyCharacterStatus = "unknown"
-}
-
-// MARK: - conformances
-extension RickAndMortyCharacterStatus: Codable {
-  init(from decoder: Decoder) throws {
-    let container = try decoder.singleValueContainer()
-    let rawValue = try container.decode(String.self)
-    self.init(rawValue: rawValue)
-  }
-
-  func encode(to encoder: Encoder) throws {
-    var container = encoder.singleValueContainer()
-    try container.encode(rawValue)
-  }
-}
-
-extension RickAndMortyCharacterStatus: CustomStringConvertible {
-  var description: String { rawValue }
-}
-
-extension RickAndMortyCharacterStatus: ExpressibleByStringLiteral {
-  init(stringLiteral rawValue: String) {
-    self.init(rawValue: rawValue)
-  }
 }
---
+++ Libs/RickAndMortyEpisodesLib/Sources/StringRepresentable.swift
@@ -0,0 +1,30 @@
+import Foundation
+
+/// A protocol for types that represent entities using an underlying String value.
+///
+/// Adopt this protocol to create distinct types backed by strings, enabling type safety, clear separation between entities, and the ability to define shared constants or utility methods.
+protocol StringRepresentable: Sendable, Equatable, Codable,
+  CustomStringConvertible, ExpressibleByStringLiteral
+{
+  var rawValue: String { get set }
+  init(rawValue: String)
+}
+
+extension StringRepresentable {
+  init(from decoder: Decoder) throws {
+    let container = try decoder.singleValueContainer()
+    let rawValue = try container.decode(String.self)
+    self.init(rawValue: rawValue)
+  }
+
+  func encode(to encoder: Encoder) throws {
+    var container = encoder.singleValueContainer()
+    try container.encode(rawValue)
+  }
+
+  var description: String { rawValue }
+
+  init(stringLiteral rawValue: String) {
+    self.init(rawValue: rawValue)
+  }
+}
