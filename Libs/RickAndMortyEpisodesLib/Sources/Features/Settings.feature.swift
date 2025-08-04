6892c399d82fd747d099ade82824a5a647abe7db
Anton Myronov <antonvmironov@gmail.com>
2025-08-04 23:08:26 +0200

    increment

--- Libs/RickAndMortyEpisodesLib/Sources/Features/Settings.feature.swift
+++ Libs/RickAndMortyEpisodesLib/Sources/Features/Settings.feature.swift
@@ -17,14 +17,15 @@ enum SettingsFeature: Feature {
 
     var body: some View {
       List {
-        header
         LabeledContent("Version", value: store.version)
+        aboutSection
+        cacheSection
       }
       .listStyle(.plain)
       .navigationTitle("Settings")
     }
 
-    var header: some View {
+    private var aboutSection: some View {
       Section(
         content: {
           Text(
@@ -41,6 +42,84 @@ enum SettingsFeature: Feature {
         }
       )
     }
+
+    private var cacheSection: some View {
+      Section(
+        content: {
+          ForEach(store.cacheReports) { report in
+            cacheReportView(report: report)
+          }
+        },
+        header: {
+          HStack {
+            Text("Cache")
+            Spacer()
+            Button(
+              action: {
+                store.send(.updateCacheReports)
+              },
+              label: {
+                Label(
+                  title: {
+                    Text("Reload")
+                  },
+                  icon: {
+                    Image(
+                      systemName:
+                        "arrow.trianglehead.2.counterclockwise.rotate.90"
+                    )
+                  }
+                )
+              }
+            )
+          }
+        }
+      )
+    }
+
+    private func cacheReportView(report: URLCacheReport) -> some View {
+      Group {
+        HStack {
+          Text("`\(report.category)` cache")
+          Spacer(minLength: UIConstants.space)
+          Button(
+            action: {
+              store.send(.clearCache(report.category))
+            },
+            label: {
+              Label(
+                title: {
+                  Text("Clear")
+                },
+                icon: {
+                  Image(systemName: "clear.fill")
+                }
+              )
+            }
+          )
+        }
+        LabeledContent(
+          "RAM used",
+          value: report.currentMemoryUsage,
+          format: .byteCount(style: .memory)
+        )
+        LabeledContent(
+          "RAM max",
+          value: report.memoryCapacity,
+          format: .byteCount(style: .memory)
+        )
+        LabeledContent(
+          "disk used",
+          value: report.currentDiskUsage,
+          format: .byteCount(style: .memory)
+        )
+        LabeledContent(
+          "disk max",
+          value: report.diskCapacity,
+          format: .byteCount(style: .memory)
+        )
+      }
+    }
   }
 
   @Reducer
@@ -48,9 +127,18 @@ enum SettingsFeature: Feature {
     typealias State = FeatureState
     typealias Action = FeatureAction
 
+    @Dependency(\.urlCacheFactory)
+    var urlCacheFactory: URLCacheFactory
+
     var body: some ReducerOf<Self> {
       Reduce { state, action in
         switch action {
+        case .updateCacheReports:
+          state.cacheReports = urlCacheFactory.allReports()
+          return .none
+        case .clearCache(let category):
+          urlCacheFactory.clearCache(category: category)
+          return .none
         default:
           return .none
         }
@@ -63,11 +151,14 @@ enum SettingsFeature: Feature {
     var version: String =
       (Bundle.main.infoDictionary?["CFBundleShortVersionString"] as? String)
       ?? "unknown"
+
+    var cacheReports = [URLCacheReport]()
   }
 
   @CasePathable
   enum FeatureAction: Equatable {
-    // No actions for now
+    case updateCacheReports
+    case clearCache(URLCacheCategory)
   }
 }
 
