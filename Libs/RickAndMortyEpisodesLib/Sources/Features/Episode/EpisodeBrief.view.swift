64efcd37abd128c077891a73d897310f2572062a
Anton Myronov <antonvmironov@gmail.com>
2025-08-07 13:20:56 +0200

    increment

---
+++ Libs/RickAndMortyEpisodesLib/Sources/Features/Episode/EpisodeBrief.view.swift
@@ -0,0 +1,56 @@
+import Foundation
+import SharedLib
+import SwiftUI
+
+extension EpisodeBriefFeature {
+  struct FeatureView: View {
+    var state: FeatureState
+
+    init(state: FeatureState) {
+      self.state = state
+    }
+
+    var body: some View {
+      VStack {
+        HStack {
+          Text("\(state.episode.name)")
+            .font(.title3)
+            .skeletonDecoration(isEnabled: state.isPlaceholder)
+          Spacer()
+        }
+        HStack {
+          Text("\(state.episode.episode)")
+            .font(.caption)
+            .fontDesign(.monospaced)
+            .skeletonDecoration(isEnabled: state.isPlaceholder)
+          Text("\(BaseEpisodeFeature.formatAirDate(episode: state.episode))")
+            .font(.caption)
+            .fontDesign(.monospaced)
+            .skeletonDecoration(isEnabled: state.isPlaceholder)
+          Spacer()
+        }
+      }
+    }
+  }
+
+  struct FeatureState: Equatable {
+    var episode: EpisodeDomainModel
+    var isPlaceholder: Bool
+
+    static func preview(isPlaceholder: Bool) -> Self {
+      .init(episode: .dummy, isPlaceholder: isPlaceholder)
+    }
+  }
+}
+
+#Preview {
+  VStack {
+    EpisodeBriefFeature.FeatureView(
+      state: .preview(isPlaceholder: true)
+    )
+    EpisodeBriefFeature.FeatureView(
+      state: .preview(isPlaceholder: false)
+    )
+  }
+  .frame(maxWidth: .infinity)
+}
